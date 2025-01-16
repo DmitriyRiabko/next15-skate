c4c7a4a5ac1ddb45a407773011f727ee1d4b9311
vsegda_vinovat <riabko97@gmail.com>
2025-01-16 19:38:48 +0200

    textandImage slice

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -4,7 +4,10 @@ import type * as prismic from "@prismicio/client";
 
 type Simplify<T> = { [KeyType in keyof T]: T[KeyType] };
 
-type HomepageDocumentDataSlicesSlice = ProductGridSlice | HeroSlice;
+type HomepageDocumentDataSlicesSlice =
+  | TextAndImageSlice
+  | ProductGridSlice
+  | HeroSlice;
 
 /**
  * Content for Homepage documents
--- src/slices/TextAndImage/index.tsx
+++ src/slices/TextAndImage/index.tsx
@@ -1,23 +1,56 @@
+import { Bounded } from "@/components/bounded";
+import { ButtonLink } from "@/components/button-link";
+import { Heading } from "@/components/heading";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import clsx from "clsx";
+import { ParallaxImage } from "./parallax-image";
 
-/**
- * Props for `TextAndImage`.
- */
 export type TextAndImageProps = SliceComponentProps<Content.TextAndImageSlice>;
 
-/**
- * Component for "TextAndImage" Slices.
- */
 const TextAndImage = ({ slice }: TextAndImageProps): JSX.Element => {
+  const theme = slice.primary.theme;
+
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className={clsx(
+        theme === "Blue" && "bg-texture bg-brand-blue text-white",
+        theme === "Lime" && "bg-texture bg-brand-lime text-white",
+        theme === "Orange" && "bg-texture bg-brand-orange text-white",
+        theme === "Navy" && "bg-texture bg-brand-navy text-white"
+      )}
     >
-      Placeholder component for text_and_image (variation: {slice.variation})
-      Slices
-    </section>
+      <div className="grid grid-cols-1 items-center gap-12 md:grid-cols-2 md:gap-24">
+        <div
+          className={clsx(
+            "flex flex-col items-center gap-8 text-center md:items-start md:text-left",
+            slice.variation === "imageOnLeft" && "md:order-2"
+          )}
+        >
+          <Heading size="lg" as="h2">
+            <PrismicRichText field={slice.primary.heading} />
+          </Heading>
+
+          <div className="max-w-md text-lg leading-relaxed">
+            <PrismicRichText field={slice.primary.body} />
+          </div>
+          <ButtonLink
+            color={theme === "Lime" ? "orange" : "lime"}
+            field={slice.primary.button}
+          >
+            {slice.primary.button.text}
+          </ButtonLink>
+        </div>
+
+        <ParallaxImage
+          foregroundImage={slice.primary.foreground_image}
+          backgroundImage={slice.primary.background_image}
+        />
+      </div>
+    </Bounded>
   );
 };
 
