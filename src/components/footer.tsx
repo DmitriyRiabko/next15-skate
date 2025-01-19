f741af839cd94e08545d66582e71c1d789795b66
vsegda_vinovat <riabko97@gmail.com>
2025-01-19 22:52:11 +0200

    added footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,45 @@
+import { createClient } from "@/prismicio";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import React from "react";
+import { Logo } from "@/components/logo";
+import { Bounded } from "./bounded";
+import { FooterPhysics } from "./footer-physics";
+import { asImageSrc } from "@prismicio/client";
+
+interface Props {
+  className?: string;
+}
+
+export const Footer: React.FC<Props> = async ({ className }) => {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+
+  const boardTextureUrls = settings.data.footer_skateboards
+    .map((item) => asImageSrc(item.skateboard, { h: 600 }))
+    .filter((url): url is string => Boolean(url));
+
+  return (
+    <footer className="bg-texture bg-zinc-900 text-white ">
+      <div className="relative h-[75vh] ~p-10/16 md:aspect-auto">
+        <PrismicNextImage
+          field={settings.data.footer_image}
+          alt=""
+          fill
+          className="object-cover"
+          width={1200}
+        />
+        <FooterPhysics boardTextureURLs={boardTextureUrls} className="absolute inset-0 overflow-hidden"/>
+        <Logo className="pointer-events-none relative h-20 mix-blend-difference" />
+      </div>
+      <Bounded as="nav">
+        <ul className="flex flex-wrap justify-center gap-8 ~text-lg/xl">
+          {settings.data.navigation.map((item) => (
+            <li key={item.link.text} className="hover:underline">
+              <PrismicNextLink field={item.link} />
+            </li>
+          ))}
+        </ul>
+      </Bounded>
+    </footer>
+  );
+};
