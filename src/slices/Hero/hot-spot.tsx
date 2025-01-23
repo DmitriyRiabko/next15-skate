6b885709c3b221334905ccee37ad3a73123de659
vsegda_vinovat <riabko97@gmail.com>
2025-01-23 22:56:19 +0200

    created hot spot for animations

---
+++ src/slices/Hero/hot-spot.tsx
@@ -0,0 +1,38 @@
+import { Billboard } from "@react-three/drei";
+import React, { useRef } from "react";
+import * as THREE from "three";
+
+interface Props {
+  className?: string;
+  position: [number, number, number];
+  isVisible: boolean;
+  color?: string;
+}
+
+export const HotSpot: React.FC<Props> = ({
+  className,
+  position,
+  isVisible,
+  color = "#E6FC6A",
+}) => {
+  const hotSpotRef = useRef<THREE.Mesh>(null);
+
+  return (
+    <Billboard position={position} follow={true}>
+      <mesh ref={hotSpotRef} visible={isVisible}>
+        <circleGeometry args={[0.02, 32]}></circleGeometry>
+        <meshStandardMaterial color={color} transparent opacity={1} />
+      </mesh>
+
+      <mesh
+        onPointerOver={() => (document.body.style.cursor = "pointer")}
+        onPointerOut={() => (document.body.style.cursor = "default")}
+
+        visible={isVisible}
+      >
+        <circleGeometry args={[0.03, 32]}></circleGeometry>
+        <meshBasicMaterial color={color} />
+      </mesh>
+    </Billboard>
+  );
+};
